fe1025c9abb5768e4d86d87174e9e8f3ee624a26
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2022-07-06 01:52:52 +0200

    support new avr dx series

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 crosstool-NG settings for a statically-linked AVR toolchain.
 
-The toolchain includes GDB and patched avr-size, that shows program and data usage relative to the target microcontroller.
+The toolchain includes GDB and patched avr-size, that shows program and data usage relative to the target microcontroller. It also includes support for the new AVR Dx series of microcontrollers.
 
 Prebuilt binaries can be downloaded here: https://distfiles.rgm.io/avr-toolchain/
--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,18 +3,35 @@
 set -ex
 
 pushd "${BUILDDIR}" > /dev/null
+
 wget "https://github.com/crosstool-ng/crosstool-ng/archive/${CT_NG_COMMIT}.tar.gz"
+wget "http://packs.download.atmel.com/Atmel.AVR-Dx_DFP.${AVR_DX_PACK_VERSION}.atpack"
+
 tar -xf "${CT_NG_COMMIT}.tar.gz"
 pushd "crosstool-ng-${CT_NG_COMMIT}/" > /dev/null
 ./bootstrap
 ./configure --enable-local
 ${MAKE_CMD}
+
 popd > /dev/null
 popd > /dev/null
 
 pushd "${SRCDIR}" > /dev/null
 "${BUILDDIR}/crosstool-ng-${CT_NG_COMMIT}/ct-ng" defconfig
 CT_PREFIX="${BUILDDIR}/prefix" "${BUILDDIR}/crosstool-ng-${CT_NG_COMMIT}/ct-ng" build
+
+mkdir -p "${BUILDDIR}/pack"
+pushd "${BUILDDIR}/pack" > /dev/null
+unzip "${BUILDDIR}/Atmel.AVR-Dx_DFP.${AVR_DX_PACK_VERSION}.atpack"
+cp -v include/avr/ioavr* "${BUILDDIR}/prefix/avr/avr/include/avr/"
+cp -v gcc/dev/*/device-specs/* "${BUILDDIR}"/prefix/avr/lib/gcc/avr/*/device-specs/
+for d in gcc/dev/*/avr*; do
+    dir="$(basename $d)"
+    mkdir -p "${BUILDDIR}/prefix/avr/avr/lib/${dir}/"
+    cp -v "${d}"/* "${BUILDDIR}/prefix/avr/avr/lib/${dir}/"
+done
+popd > /dev/null
+
 tar -cJvf "${BUILDDIR}/${PN}-${TARGET/dist-/}-${PV}.tar.xz" -C "${BUILDDIR}/prefix" avr
 popd > /dev/null
 
